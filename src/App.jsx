b64b57549520a79b357641a49a2eaa1c1194c255
Magno Valdetaro <mvaldetaro@gmail.com>
2023-10-30 11:17:52 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { CrossTab } from "../";
+import { Alert, ThemeProvider } from "../dist/mui";
+import { themeDarkPrimary } from "../dist/styles";
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={themeDarkPrimary}>
+      <Alert>This is an error alert — check it out!</Alert>
+      <CrossTab appName="TESTE" />
       <div>Hello there!</div>
-    </>
+    </ThemeProvider>
   );
 }
 
