c83a5c20a128faa7357924dcea0dc99644a84778
Magno Valdetaro <mvaldetaro@gmail.com>
2023-11-01 11:03:34 -0300

    FilterBar, FluidPaper, Format

---
+++ lib/components/core/FluidPaper/FluidPaper.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import { Stack } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const Root = styled(Stack)(({ selected, theme }) => ({
+    color: 'rgba(255, 255, 255, 0.87)',
+    transition: 'all 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+    backgroundColor: theme.palette.secondary.main,
+    alignItems: 'center',
+    flexDirection: 'row',
+    height: '100%',
+    margin: '0px 16px',
+    borderRadius: '10px',
+    boxShadow:
+        '0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12)',
+    ...(selected && {
+        boxShadow: 'none',
+        borderRadius: 0,
+        width: '100vw',
+        backgroundColor: theme.palette.secondary.dark,
+        margin: 0,
+        transition: 'all 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms'
+    })
+}));
+function FluidPaper(props) {
+    return (
+        <Root selected={Boolean(props.selected)} onClick={props.onClick}>
+            {props.children}
+        </Root>
+    );
+}
+
+FluidPaper.propTypes = {
+    children: PropTypes.node,
+    square: PropTypes.bool,
+    variant: PropTypes.string,
+    elevation: PropTypes.string,
+    selected: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
+    onClick: PropTypes.func
+};
+
+export default FluidPaper;
---
+++ lib/components/core/Format/Format.jsx
@@ -0,0 +1,68 @@
+import PropTypes from 'prop-types';
+import { Typography, Rate } from '@mui/material';
+import { formats } from 'investira.sdk';
+
+function Format(props) {
+    const formatValue = (pType, pValue, pDecimais) => {
+        const xValue = {
+            ...(['currency', 'currency_abs', 'number', 'percentual'].includes(props.format) && {
+                currency:
+                    typeof pValue === 'string'
+                        ? pValue
+                        : formats.friendlyNumber(pValue, pDecimais || 2, true),
+                currency_abs:
+                    typeof pValue === 'string'
+                        ? pValue
+                        : formats.formatNumber(pValue, 2, true, true),
+                number:
+                    typeof pValue === 'string'
+                        ? pValue
+                        : formats.formatNumber(pValue, pDecimais || 0, true, false)
+            }),
+            ...(['date', 'hour'].includes(props.format) && {
+                date: formats.formatDate(pValue),
+                hour:
+                    typeof pValue === 'string' ? pValue : formats.formatDateCustom(pValue, 'HH:mm')
+            }),
+            rate: <Rate value={pValue} status={props.status} size={props.size} />,
+            text: pValue,
+            percentual: `${pValue}%`
+        };
+
+        return xValue[pType] || pValue;
+    };
+
+    const { format, value, decimais, ...restProps } = props;
+
+    return <Typography {...restProps}>{formatValue(format, value, decimais)}</Typography>;
+}
+
+Format.propTypes = {
+    format: PropTypes.oneOf([
+        'currency',
+        'currency_abs',
+        'number',
+        'percentual',
+        'rate',
+        'date',
+        'hour',
+        'text'
+    ]),
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.object]),
+    size: PropTypes.number,
+    variant: PropTypes.string,
+    color: PropTypes.string,
+    status: PropTypes.oneOf([0, 1, 2]),
+    decimais: PropTypes.number
+};
+
+Format.defaultProps = {
+    format: 'text',
+    value: 0,
+    color: 'textPrimary',
+    size: 16,
+    variant: 'caption',
+    status: 1
+};
+
+export default Format;
