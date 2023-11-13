635a38b93aded5aef35fa8b53598b4fee4a57b27
Magno Valdetaro <mvaldetaro@gmail.com>
2023-11-13 09:16:06 -0300

    Funcionando

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import fs from 'fs/promises';
+import path from 'path';
+import url from 'url';
+import { createRequire } from 'module';
+
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [react(), svgr(), reactVirtualized()],
 
     build: {
         copyPublicDir: false,
         lib: {
             entry: {
-                core: resolve(__dirname, 'lib/core.js'),
-                mui: resolve(__dirname, 'lib/mui.js'),
-                themes: resolve(__dirname, 'lib/themes.js'),
-                lib: resolve(__dirname, 'lib/lib.js')
+                core: path.resolve(__dirname, 'lib/core.js'),
+                mui: path.resolve(__dirname, 'lib/mui.js'),
+                themes: path.resolve(__dirname, 'lib/themes.js'),
+                lib: path.resolve(__dirname, 'lib/lib.js')
                 // charts: resolve(__dirname, "lib/charts.js"),
                 // reports: resolve(__dirname, "lib/reports.js"),
             },
@@ -30,7 +34,7 @@ export default defineConfig({
                 'react-redux',
                 'redux',
                 'react-router-dom',
-                // 'react-virtualized',
+                'react-virtualized',
                 'redux-persist',
                 'redux-persist-pouchdb',
                 'redux-thunk',
@@ -54,7 +58,7 @@ export default defineConfig({
                     'react-dnd-html5-backend': 'reactDndHtml5Backend',
                     'react-dnd-touch-backend': 'reactDndTouchBackend',
                     'react-dom': 'ReactDOM',
-                    // 'react-virtualized': 'reactVirtualized',
+                    'react-virtualized': 'reactVirtualized',
                     'react/jsx-runtime': 'jsxRuntime',
                     redux: 'redux',
                     'react-router-dom': 'reactRouterDom',
@@ -87,3 +91,33 @@ export default defineConfig({
         }
     }
 });
+
+// Solução: https://github.com/bvaughn/react-virtualized/issues/1632
+const WRONG_CODE = `import { bpfrpt_proptype_WindowScroller } from "../WindowScroller.js";`;
+
+function reactVirtualized() {
+    return {
+        name: 'flat:react-virtualized',
+        // Observação: não podemos usar o hook `transform` aqui
+        // porque as bibliotecas são pré-agrupadas diretamente no Vite,
+        // os plugins não conseguem manipular essa etapa no momento.
+        // em vez disso, editamos manualmente o arquivo em node_modules.
+        // tudo o que precisamos é encontrar o momento antes do pré-agrupamento.
+
+        configResolved: async () => {
+            const require = createRequire(import.meta.url);
+            const reactVirtualizedPath = require.resolve('react-virtualized');
+            const { pathname: reactVirtualizedFilePath } = new url.URL(
+                reactVirtualizedPath,
+                import.meta.url
+            );
+            const file = reactVirtualizedFilePath.replace(
+                path.join('dist', 'commonjs', 'index.js'),
+                path.join('dist', 'es', 'WindowScroller', 'utils', 'onScroll.js')
+            );
+            const code = await fs.readFile(file, 'utf-8');
+            const modified = code.replace(WRONG_CODE, '');
+            await fs.writeFile(file, modified);
+        }
+    };
+}
