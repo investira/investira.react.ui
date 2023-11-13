ab837f94209e5aef3584a2cefc07d5b4f83b4627
Magno Valdetaro <mvaldetaro@gmail.com>
2023-11-13 08:51:17 -0300

    Funcionando

--- lib/components/core/CopyToClipboard/CopyToClipboard.jsx
+++ lib/components/core/CopyToClipboard/CopyToClipboard.jsx
@@ -1,74 +1,74 @@
-import { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import Tooltip from "@mui/material";
-import { styled } from "@mui/material/styles";
-import Icon from "../Icon";
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Tooltip } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import Icon from '../Icon';
 
-const Button = styled("button")(() => ({
-  display: "inline-block",
-  position: "relative",
-  pointerEvents: "all",
-  verticalAlign: "top",
-  border: 0,
-  height: "16px",
-  width: "16px",
-  margin: "0 4px",
-  "& > svg": {
-    position: "absolute",
-    top: 0,
-    left: 0,
-  },
+const Button = styled('button')(() => ({
+    display: 'inline-block',
+    position: 'relative',
+    pointerEvents: 'all',
+    verticalAlign: 'top',
+    border: 0,
+    height: '16px',
+    width: '16px',
+    margin: '0 4px',
+    '& > svg': {
+        position: 'absolute',
+        top: 0,
+        left: 0
+    }
 }));
 
 function CopyToClipboard(props) {
-  const [iconName, setIconName] = useState("paper");
-  const [tooltip, setTooltip] = useState("Copiar");
+    const [iconName, setIconName] = useState('paper');
+    const [tooltip, setTooltip] = useState('Copiar');
 
-  let body = document.getElementsByTagName("body")[0];
+    let body = document.getElementsByTagName('body')[0];
 
-  let timeout = null;
+    let timeout = null;
 
-  const copyToClipboard = (pValue, pEvent) => {
-    pEvent.stopPropagation();
-    const tempInput = document.createElement("input");
-    body.appendChild(tempInput);
-    tempInput.setAttribute("value", pValue.toString());
-    tempInput.select();
-    document.execCommand("copy");
-    body.removeChild(tempInput);
+    const copyToClipboard = (pValue, pEvent) => {
+        pEvent.stopPropagation();
+        const tempInput = document.createElement('input');
+        body.appendChild(tempInput);
+        tempInput.setAttribute('value', pValue.toString());
+        tempInput.select();
+        document.execCommand('copy');
+        body.removeChild(tempInput);
 
-    setIconName("ok");
-    setTooltip("COPIADO!");
-  };
-
-  const resetState = () => {
-    timeout = setTimeout(() => {
-      setIconName("paper");
-      setTooltip("Copiar");
-    }, 1000);
-  };
-
-  useEffect(() => {
-    resetState();
-  }, [iconName]);
-
-  useEffect(() => {
-    return () => {
-      clearTimeout(timeout);
+        setIconName('ok');
+        setTooltip('COPIADO!');
     };
-  }, []);
 
-  return (
-    <Tooltip title={tooltip} placement="top">
-      <Button onClick={(e) => copyToClipboard(props.value, e)}>
-        <Icon iconName={iconName} size={16} color={"primary"} />
-      </Button>
-    </Tooltip>
-  );
+    const resetState = () => {
+        timeout = setTimeout(() => {
+            setIconName('paper');
+            setTooltip('Copiar');
+        }, 1000);
+    };
+
+    useEffect(() => {
+        resetState();
+    }, [iconName]);
+
+    useEffect(() => {
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, []);
+
+    return (
+        <Tooltip title={tooltip} placement="top">
+            <Button onClick={e => copyToClipboard(props.value, e)}>
+                <Icon iconName={iconName} size={16} color={'primary'} />
+            </Button>
+        </Tooltip>
+    );
 }
 
 CopyToClipboard.propTypes = {
-  value: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired
 };
 
 export default CopyToClipboard;
--- lib/components/core/DndProvider/DndProvider.jsx
+++ lib/components/core/DndProvider/DndProvider.jsx
@@ -1,7 +1,7 @@
 import { DndProvider as WarpDndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { TouchBackend } from 'react-dnd-touch-backend';
-import { isTouchDevice } from '../../../utils/helpers';
+import { isTouchDevice } from '../../utils/helpers';
 
 const DndProvider = props => {
     const backendForDND = isTouchDevice() ? TouchBackend : HTML5Backend;
