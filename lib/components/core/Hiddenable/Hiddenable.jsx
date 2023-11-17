241e4ac0de6fd7be62953392659b6170bc43efcc
Magno Valdetaro <mvaldetaro@gmail.com>
2023-11-17 17:54:17 -0300

    Funcionando

---
+++ lib/components/core/Hiddenable/Hiddenable.jsx
@@ -0,0 +1,17 @@
+//import React from 'react';
+import PropTypes from 'prop-types';
+
+const Hiddenable = props => {
+    return props.visible ? props.children : null;
+};
+
+Hiddenable.propTypes = {
+    visible: PropTypes.bool,
+    children: PropTypes.node.isRequired
+};
+
+Hiddenable.defaultProps = {
+    visible: true
+};
+
+export default Hiddenable;
