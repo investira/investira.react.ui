ee74caa0c4f3a820e3c700a7cede0a6762d7f7b8
Magno Valdetaro <mvaldetaro@gmail.com>
2023-11-17 10:58:09 -0300

    Funcionando

---
+++ lib/components/core/NavDots/NavDots.jsx
@@ -0,0 +1,108 @@
+import { useState, useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Box, ButtonBase } from '@mui/material';
+
+function NavDots(props) {
+    const [wrapWidth, setWrapWidth] = useState(window.screen.width);
+    const [wrapPosition, setWrapPosition] = useState(0);
+
+    const btnRef = useRef();
+
+    const handleClick = pIndex => {
+        props.onClick && props.onClick(pIndex);
+    };
+
+    const navigation = (pSize, pIndex, pDotWidth) => {
+        const xTotalDots = pSize;
+        const xTotalVisible = 5;
+        const xOffset = 3;
+        const xIntervalo = xTotalDots - xTotalVisible;
+        const xWalker = pIndex + xOffset;
+        const xMultiply = xTotalDots - (xWalker + xIntervalo);
+
+        if (pIndex >= xTotalVisible - xOffset && pIndex <= xTotalDots - xOffset) {
+            const xNewPos = pDotWidth * xMultiply;
+            setWrapPosition(xNewPos);
+        } else if (pIndex < xTotalVisible - xOffset) {
+            const xStartPos = 0;
+            setWrapPosition(xStartPos);
+        } else if (pIndex > xTotalDots - xOffset) {
+            const xEndPos = (xTotalDots - xTotalVisible) * pDotWidth;
+            setWrapPosition(xEndPos * -1);
+        }
+    };
+
+    useEffect(() => {
+        setWrapWidth(props.size > 5 ? btnRef.current.offsetWidth * props.size : '100%');
+    }, []);
+
+    useEffect(() => {
+        if (props.size > 5) {
+            navigation(props.size, props.index, btnRef.current.offsetWidth);
+        }
+    }, [props.index]);
+
+    const xChilds = [];
+
+    for (let xI = 0; xI < props.size; xI++) {
+        xChilds.push(
+            <div ref={btnRef} key={xI}>
+                <ButtonBase centerRipple={true} onClick={() => handleClick(xI)}>
+                    <Box py={1} px={'13px'}>
+                        <Box
+                            component="span"
+                            sx={[
+                                {
+                                    display: 'inline-block',
+                                    p: 0.5,
+                                    borderRadius: '50%',
+                                    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+                                    transition: 'all 0.2s ease-in-out'
+                                },
+                                props.index === xI && {
+                                    backgroundColor: '#00dfa8',
+                                    transform: 'scale(1.5)'
+                                }
+                            ]}
+                        />
+                    </Box>
+                </ButtonBase>
+            </div>
+        );
+    }
+
+    return (
+        <Box
+            sx={{
+                position: 'relative',
+                width: '170px',
+                overflow: 'hidden',
+                color: 'rgba(255, 255, 255, 0.9)'
+            }}>
+            <Box
+                sx={[
+                    {
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        transition: 'all 0.2s ease-in-out'
+                    },
+                    props.size > 5 && { justifyContent: 'flex-start' }
+                ]}
+                style={{
+                    width: wrapWidth,
+                    transform: `translate(${wrapPosition}px,0px)`
+                }}>
+                {xChilds}
+            </Box>
+        </Box>
+    );
+}
+
+NavDots.propTypes = {
+    onClick: PropTypes.func,
+    size: PropTypes.number,
+    index: PropTypes.number
+};
+
+export default NavDots;
---
+++ lib/components/core/OverWaiting/OverWaiting.jsx
@@ -0,0 +1,122 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Backdrop, Typography, LinearProgress, Button, Box, Stack } from '@mui/material';
+import { Icon } from '../';
+
+function OverWaiting(props) {
+    const [open, setOpen] = React.useState(false);
+
+    const xSxRoot = {
+        zIndex: 'drawer' + 1,
+        ...(props.backgroundFlat && { backgroundColor: 'background.default' })
+    };
+
+    const handleCancel = pEvent => {
+        props.onCancel && props.onCancel(pEvent);
+    };
+
+    useEffect(() => {
+        setOpen(props.open);
+    }, [props.open]);
+
+    const { message, progressProps, typographyProps, header } = props;
+
+    return (
+        <Backdrop sx={xSxRoot} open={open}>
+            {open && (
+                <>
+                    {header && (
+                        <Box
+                            sx={{
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                right: 0,
+                                height: '55px',
+                                padding: '16px'
+                            }}>
+                            {header}
+                        </Box>
+                    )}
+                    <Box
+                        sx={{
+                            margin: '0 auto',
+                            width: '80%',
+                            textAlign: 'center',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center'
+                        }}>
+                        <Box sx={{ position: 'relative' }}>
+                            <Icon
+                                color={progressProps.color || 'primary'}
+                                iconName="clock"
+                                size={128}
+                            />
+                        </Box>
+
+                        <Stack spacing={2} sx={{ paddingTop: '24px', width: '100%' }}>
+                            <LinearProgress
+                                color={progressProps.color}
+                                variant={progressProps.variant}
+                                value={progressProps.value}
+                            />
+
+                            {message && (
+                                <Typography
+                                    color={typographyProps.color || 'textPrimary'}
+                                    align="center"
+                                    variant={typographyProps.variant || 'caption'}>
+                                    {message}
+                                </Typography>
+                            )}
+                        </Stack>
+
+                        {props.cancelable && (
+                            <Box sx={{ position: 'absolute', bottom: '24px' }}>
+                                <Button variant="outlined" color="primary" onClick={handleCancel}>
+                                    Cancelar
+                                </Button>
+                            </Box>
+                        )}
+                    </Box>
+                </>
+            )}
+        </Backdrop>
+    );
+}
+
+OverWaiting.propTypes = {
+    open: PropTypes.bool,
+    min: PropTypes.number,
+    max: PropTypes.number,
+    message: PropTypes.string,
+    progressProps: PropTypes.shape({
+        color: PropTypes.oneOf(['primary', 'secondary']),
+        value: PropTypes.number,
+        variant: PropTypes.oneOf(['buffer', 'determinate', 'indeterminate', 'query'])
+    }),
+    typographyProps: PropTypes.object,
+    onCancel: PropTypes.func,
+    cancelable: PropTypes.bool,
+    backgroundFlat: PropTypes.bool,
+    header: PropTypes.node
+};
+
+OverWaiting.defaultProps = {
+    open: false,
+    min: 0,
+    max: 100,
+    progressProps: {
+        color: 'primary',
+        value: 100,
+        variant: 'indeterminate'
+    },
+    typographyProps: {
+        color: 'textPrimary',
+        variant: 'caption'
+    },
+    cancelable: true
+};
+
+export default OverWaiting;
